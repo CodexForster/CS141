9cba8e5b32518585b86766a796c20a89ddc6aa74
Danush-shekar <31274364+Danush-shekar@users.noreply.github.com>
2017-11-19 13:16:37 +0530

    Update lab7prog5.123.cpp

--- lab7prog5.123.cpp
+++ lab7prog5.123.cpp
@@ -1,21 +1,55 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-/* Answer for 5.1:
-void create(char,int,int,int);
-*/
 
-/*Answer for 5.2:
-int daysSinceLastWeighed(int today)
-*/
+class ZooAnimal {
+private:
+	char* name;
+	int cage_number;
+	int weight_date;
+	int weight;
+public:
+	void Destroy();
+	void Create(char* n, int cn, int wd, int w);
+	
+	char* reptName();
+	int daysSinceLastWeighed(int today);
+};
 
-/*Answer for 5.3:
---> Declare a variable : char bname;
---> Assign it as       : bname=*bozo.reptname();
--->Print it            :  cout << "This animal's name is " << bname << endl;
-*/
+void ZooAnimal::Destroy() {
+	delete [] name;
+}
 
+void ZooAnimal::Create(char* n, int cn, int wd, int w) {
+	name = n;
+	cage_number = cn;
+	weight_date = wd;
+	weight = w;
+}
 
+char* ZooAnimal::reptName() {
+	return name;
+}
+
+int ZooAnimal::daysSinceLastWeighed(int today) {
+	int startday, thisday;
+	thisday = today/100*30 + today - today/100*100;
+	startday = weight_date/100*30 + weight_date - weight_date/100*100;
+	
+	if (thisday < startday) {
+		thisday += 360;
+	}
+	
+	return (thisday - startday);
+}
 
 int main() {
+	ZooAnimal bozo;
+	char name[50] = "Bozo";
+	
+	bozo.Create (name, 408, 1027, 400);
+	
+	cout << "This animal's name is " << bozo.reptName() << endl;
+	
+	bozo.Destroy();
 	return 0;
 }
